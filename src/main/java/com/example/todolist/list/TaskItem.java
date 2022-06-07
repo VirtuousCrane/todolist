537e938a465cc02b83af5e1838cdad85366628dd
CJDFINTECH\chayut.li <chayut.li@cjdfintech.com>
2022-06-07 13:51:20 +0700

    Added constructors

--- src/main/java/com/example/todolist/list/TaskItem.java
+++ src/main/java/com/example/todolist/list/TaskItem.java
@@ -4,15 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.scheduling.config.Task;
 
 import javax.persistence.*;
 
 @Table
 @Entity
-@Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TaskItem {
 
     @Id
@@ -32,4 +30,48 @@ public class TaskItem {
     @Enumerated(EnumType.STRING)
     private TaskStatus status;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskItem(Long id, String subject, String body, TaskStatus status) {
+        this.id = id;
+        this.subject = subject;
+        this.body = body;
+        this.status = status;
+    }
+
+    public TaskItem(String subject, String body, TaskStatus status) {
+        this.subject = subject;
+        this.body = body;
+        this.status = status;
+    }
 }
