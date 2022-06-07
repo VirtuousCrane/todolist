f238dbc27acc2bc671d5ac65692e5016b487dd96
Supawit <jimsupawitzz@gmail.com>
2022-06-07 15:08:43 +0700

    [Update] Taskservices unit testing

--- src/test/java/com/example/todolist/TodolistApplicationTests.java
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -13,36 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.mockito.Mockito.verify;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
+
 class TodolistApplicationTests {
-	@Mock
-	private TaskRepository taskRepository;
 
-	private TaskItemService underTest;
-
-	@BeforeEach
-	void setUp() {
-		underTest = new TaskItemService(taskRepository);
-	}
-	void canGetAllTask(){
-		underTest.getAllTasks();
-		verify(taskRepository).findAll();
-	}
-	@Test
-	void canAddTask() {
-		TaskItem taskItem = new TaskItem("Test subject",
-				"Test Body",
-				TaskStatus.DONE);
-		
-	}
-
-	@Test
-	void updateTask() {
-	}
-
-	@Test
-	void deleteTask() {
-	}
 
 }
